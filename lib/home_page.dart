dbcc5dff4a1808fe8ad03c134d9e9430795a3ede
Tiago Rodrigues <trodrigues@visualset.local>
2024-08-23 09:16:45 -0300

    salvar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:meuprimeiroprojeto/app_controller.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() {
     return HomePageState();
@@ -12,73 +11,70 @@ class HomePage extends StatefulWidget{
 class HomePageState extends State<HomePage> {
   int counter = 0;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(
+      drawer: Drawer(
         child: Column(
           children: [
-            
-            UserAccountsDrawerHeader(
-              accountName: Text:('Tiago Silva'), 
-              accountEmail: Text:('suporte2@visualset.com.br'),
-            ),
+            const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      'https://th.bing.com/th/id/OIP.hpJkZoIhk0SO08T6H5-HbAAAAA?rs=1&pid=ImgDetMain'
+                    ),
+                    radius: 40,
+                  ),
+                  const SizedBox(width: 16),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Tiago Silva',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'suporte2@visualset.com.br',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
             ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Logout'),
-              subtitle: Text('Finalizar sessão'),
-              onTap: (){
-               Navigator.of(context).pushReplacementNamed('/');
+              leading: const Icon(Icons.home),
+              title: const Text('Logout'),
+              subtitle: const Text('Finalizar sessão'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
               },
             ),
           ],
         ),
       ),
       appBar: AppBar(
-        title: Text('Tiago Silva'),
-        actions: [
+        title: const Text('Tiago Silva'),
+        actions: const [
           CustomSwitch(),
         ],
         backgroundColor: AppController.instance.isDartTheme
                 ? Colors.black // Cor de fundo para o AppBar em tema escuro
                 : Colors.red, // Cor de fundo para o AppBar em tema claro
       ),
-      
-      body: Container(
+      body: SizedBox(
         width: double.infinity,
         height: double.infinity,
         child: ListView(
           scrollDirection: Axis.vertical,
-          //mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Contador: $counter'),
-            Container(height: 10),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
-            CustomSwitch(),
-            Container(height: 20),
+            const SizedBox(height: 10),
+            const CustomSwitch(),
+            const SizedBox(height: 20),
+            const CustomSwitch(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -99,29 +95,32 @@ class HomePageState extends State<HomePage> {
                   color: Colors.black,
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
-     floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
         onPressed: () {
           setState(() {
             counter++;
           });
-          },),
+        },
+      ),
     );
   }
 }
 
 class CustomSwitch extends StatelessWidget {
+  const CustomSwitch({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Switch(
-        value: AppController.instance.isDartTheme, 
-        onChanged: (value) {
-          AppController.instance.changeTheme();
-        });
+      value: AppController.instance.isDartTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
+    );
   }
 }
